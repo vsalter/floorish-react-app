20931e604db9a5ea83bec3a734160fe876d68e33
vsalter <vsalter1@student.gsu.edu>
2023-06-07 18:17:10 -0400

    created initial navbar with brand, cart and sign in links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,23 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">Floorish</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
+                        <Nav.Link href="/login"><FaUser />Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
   )
 }
 
