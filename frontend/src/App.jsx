b7a6804fcb1fb41e3d14214bf03badb188d3e75a
vsalter <vsalter1@student.gsu.edu>
2023-06-08 16:51:30 -0400

    implement react-router-dom in index.js and app.jsx. Updated a tags to links in Product component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import { Container } from "react-bootstrap";
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
     <Header />
     <main className="py-3">
       <Container>
-        <HomePage />
+        <Outlet />
       </Container>
     </main>
     <Footer />
