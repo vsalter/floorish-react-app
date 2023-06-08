2bf91847c697e4e8e0787923a5d7836826cafbfd
vsalter <vsalter1@student.gsu.edu>
2023-06-08 18:17:17 -0400

    added ProductPage component. Added back button to ProductPage component.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,13 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
+      <Route path='/product/:id' element={<ProductPage />} />
     </Route>
   )
 );
---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,18 @@
+import { useParams } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import products from "../products";
+
+const ProductPage = () => {
+    const { id: productId } = useParams();
+    const product = products.find((p) => p._id === productId);
+    
+
+    return (
+        <>
+            <Link className="btn btn-light my-3" to="/">Go Back</Link>
+        </>
+    )
+}
+
+export default ProductPage
