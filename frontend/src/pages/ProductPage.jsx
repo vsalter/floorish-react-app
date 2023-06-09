f8cf91fb6099422a85ef6264145bbac03705cf37
vsalter <vsalter1@student.gsu.edu>
2023-06-09 14:14:00 -0400

    refactored code in ProductPage component so that data is now being fetched from the backend

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import products from "../products";
+import axios from "axios";
+
 
 const ProductPage = () => {
+    const [product, setProduct] = useState({});
+
     const { id: productId } = useParams();
-    const product = products.find((p) => p._id === productId);
+    
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+
+        fetchProduct();
+    }, [productId])
     
 
     return (
