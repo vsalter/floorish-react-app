be97cf7d0747c1a4bdb5f699d44eda79391a4c39
vsalter <vsalter1@student.gsu.edu>
2023-06-10 19:03:43 -0400

    fixed initialization error in productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,24 @@
 import express from 'express';
-import products from '../data/products.js';
+import asyncHandler from '../middleware/asyncHandler.js';
+import product from '../models/product.js';
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.json(products);
-});
 
-router.get('/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await product.find({});
+    res.json(products);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const Product = await product.findById(req.params.id);
+
+    if (Product) {
+        return res.json(Product);
+    }
+
+    res.status(404).json({ message: 'Product not found'});
+
+}));
 
 
 export default router;
