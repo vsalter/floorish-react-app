f59aa5d33a1d78e9ac14a62c56c84685e6fce7fa
vsalter <vsalter1@student.gsu.edu>
2023-06-12 19:02:51 -0400

    added functionality so that you can select what quanity of a product you would like

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Form, Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -9,6 +10,9 @@ import Message from '../components/Message';
 
 const ProductPage = () => {
     const { id: productId } = useParams();
+
+    const [qty, setQty] = useState(1);
+
     const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
     return (
@@ -45,6 +49,24 @@ const ProductPage = () => {
                                     <Col><strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong></Col>
                                 </Row>
                             </ListGroup.Item>
+
+                            {product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty:</Col>
+                                        <Col>
+                                            <Form.Control as='select' value={qty} onChange={(e) => setQty(Number(e.target.value))}>
+                                                {[...Array(product.countInStock).keys()].map((x) => (
+                                                    <option key={ x + 1 } value={ x + 1}>
+                                                        { x + 1 }
+                                                    </option>
+                                                )) }
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )}
+
                             <ListGroup.Item>
                                 <Button className="btn-block" type="button" disabled={product.countInStock === 0}>Add To Cart</Button>
                             </ListGroup.Item>
