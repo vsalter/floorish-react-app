33e4261e1f3f6a8b0c645265b74cb24534b0f98a
vsalter <vsalter1@student.gsu.edu>
2023-06-14 19:52:25 -0400

    added registration functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,8 @@ import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,6 +21,7 @@ const router = createBrowserRouter(
       <Route path='/product/:id' element={<ProductPage />} />
       <Route path='/cart' element={<CartPage />} />
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
     </Route>
   )
 );
