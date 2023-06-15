b75f78c264038ef9e890c72d6629397e5e6cd8a2
vsalter <vsalter1@student.gsu.edu>
2023-06-15 05:15:41 -0400

    create order controller and routes

---
+++ backend/controllers/orderCtlr.js
@@ -0,0 +1,36 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import order from "../models/order.js";
+
+const addOrderItems = asyncHandler(async (req, res) => {
+    res.send('add order items');
+});
+
+const getMyOrders = asyncHandler(async (req, res) => {
+    res.send('get my orders');
+});
+
+const getOrderById = asyncHandler(async (req, res) => {
+    res.send('get order by id');
+});
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    res.send('update order to paid');
+});
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    res.send('update order to delivered');
+});
+
+const getOrders = asyncHandler(async (req, res) => {
+    res.send('get all orders');
+});
+
+
+export {
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders
+}
