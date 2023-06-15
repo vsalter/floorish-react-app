5f089643bc71c2247a8d98e7865d8ec1e6793317
vsalter <vsalter1@student.gsu.edu>
2023-06-15 01:17:40 -0400

    created checkout steps component

--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from '../slices/cartSlice';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 
 
@@ -29,7 +30,10 @@ const ShippingPage = () => {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 />
+
             <h1>Shipping</h1>
+            
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address' className='my-2'>
                     <Form.Label>Address</Form.Label>
